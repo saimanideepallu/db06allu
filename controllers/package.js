ff41c5e2417e4255fd827420a3394f1276cef3c8
saimanideepallu <manideepmlalgo@gmail.com>
2021-04-09 18:10:05 -0500

    lab12

--- controllers/package.js
+++ controllers/package.js
@@ -9,9 +9,16 @@ exports.package_list = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
-// for a specific package.
-exports.package_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: package detail: ' + req.params.id);
+// for a specific Costume.
+exports.package_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await package.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle Costume create on POST.
 exports.package_create_post = async function(req, res) {
@@ -36,10 +43,24 @@ exports.package_create_post = async function(req, res) {
 exports.package_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: package delete DELETE ' + req.params.id);
 };
-// Handle package update form on PUT.
-exports.package_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: package update PUT' + req.params.id);
+//Handle Costume update form on PUT.
+exports.package_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await package.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.mode) toUpdate.mode = req.body.mode;
+        if(req.body.price) toUpdate.price = req.body.price;
+        if(req.body.quantity) toUpdate.quantity = req.body.quantity;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.package_view_all_Page = async function(req, res) {
